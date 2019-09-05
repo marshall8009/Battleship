565318a578866699a8b9800cde9cb83fe7d47c17
Jake <marshall_8009@yahoo.com>
2019-09-05 13:22:29 -0700

    Game Logic and Console App first commit

---
+++ BattleshipApp/Battleship/Program.cs
@@ -0,0 +1,73 @@
+﻿using BattleshipLibrary;
+using BattleshipLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+
+            PlayerInfoModel player1 = CreatePlayer("Player 1");
+            PlayerInfoModel player2 = CreatePlayer("Player 2");
+
+            Console.ReadLine();
+        }
+
+        private static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Battleship Lite.");
+            Console.WriteLine();
+        }
+
+        private static PlayerInfoModel CreatePlayer(string playerTitle)
+        {
+            PlayerInfoModel output = new PlayerInfoModel();
+
+            Console.WriteLine($"Player information for { playerTitle }");
+            
+            output.Name = AskForUsersName();
+
+            // Load up the Shot Grid
+            GameLogic.InitializeGrid(output);
+
+            // Ask user for ship placements
+            PlaceShips(output);
+            
+            Console.Clear();
+
+            return output;
+        }
+
+        private static string AskForUsersName()
+        {
+            Console.Write("Please enter your name: ");
+            string output = Console.ReadLine();
+
+            return output;
+        }
+
+        private static void PlaceShips(PlayerInfoModel model)
+        {
+            do
+            {
+                Console.Write($"Where do you want to place ship number { model.ShipLocations.Count + 1 }: ");
+                string location = Console.ReadLine();
+
+                bool isValidLocation = GameLogic.PlaceShip(model, location);
+
+                if (isValidLocation == false)
+                {
+                    Console.WriteLine("Not a valid location. Please try again.");
+                }
+                
+            } while (model.ShipLocations.Count < 5);
+        }
+    }
+}
--- BattleshipApp/BattleshipLibrary/Models/PlayerInfoModel.cs
+++ BattleshipApp/BattleshipLibrary/Models/PlayerInfoModel.cs
@@ -9,7 +9,7 @@ namespace BattleshipLibrary.Models
     public class PlayerInfoModel
     {
         public string Name { get; set; }
-        public List<GridSpotModel> ShipLocations { get; set; }
-        public List<GridSpotModel> ShotGrid { get; set; }
+        public List<GridSpotModel> ShipLocations { get; set; } = new List<GridSpotModel>();
+        public List<GridSpotModel> ShotGrid { get; set; } = new List<GridSpotModel>();
     }
 }
