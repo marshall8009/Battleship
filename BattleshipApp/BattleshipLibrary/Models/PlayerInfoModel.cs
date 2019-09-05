2663b2386622fdb8f942d72379dcf75102e99d35
Jake <marshall_8009@yahoo.com>
2019-09-05 12:11:29 -0700

    Class Library Commit

---
+++ BattleshipApp/BattleshipLibrary/Models/PlayerInfoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLibrary.Models
+{
+    public class PlayerInfoModel
+    {
+        public string Name { get; set; }
+        public List<GridSpotModel> ShipLocations { get; set; }
+        public List<GridSpotModel> ShotGrid { get; set; }
+    }
+}
